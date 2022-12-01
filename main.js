13c5b8c98b268fb3f662a5738124570e3260b507
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2022-12-01 13:02:25 +0900

    22..11.30_stydy

--- main.js
+++ main.js
@@ -1,39 +1,16 @@
-const gameForm = document.querySelector("#game_form");
-const getNum = document.querySelector(".num1");
-const choiceNum = document.querySelector(".num2");
-const result = document.querySelector(".game_result");
-const choiceNum1 = document.querySelector(".num1_a");
-const choiceNum2 = document.querySelector(".num2_a");
-const resultText = document.querySelector(".result");
+const clockTitle = document.querySelector(".js-clock");
 
-const HIDDEN_CLASSNAME = "hidden";
-const GETNUM_KEY = "num1";
-const CHOICENUM_KEY = "num2";
+function clock() {
+    const masTime = new Date("2022-12-25");
+    const todayTime = new Date();
+    
+    const diff = masTime - todayTime;
 
-function onGameFormSubmit(event) {
-  event.preventDefault();
-  const num1 = getNum.value;
-  localStorage.setItem(GETNUM_KEY, num1);
-  const num2 = choiceNum.value;
-  localStorage.setItem(CHOICENUM_KEY, num2);
-  const savedNum1 = localStorage.getItem(CHOICENUM_KEY);
-  choiceNum1.innerText = `${savedNum1}`;
-  const savedNum2 = localStorage.getItem(GETNUM_KEY);
-  const getRandomNum = Math.floor(Math.random() * savedNum2);
-  choiceNum2.innerText = `${getRandomNum}`;
-  console.log(savedNum1);
-  console.log(getRandomNum);
-  if (+savedNum1 >= savedNum2) {
-    alert("choice  again");
-  } else {
-    if (+savedNum1 === getRandomNum) {
-      resultText.innerText = `You won!`;
-    } else {
-      resultText.innerText = `You lost!`;
-    }
-    result.classList.remove(HIDDEN_CLASSNAME);
-    console.dir(resultText);
-  }
+    const dDay = Math.floor(diff / (1000*60*60*24));
+    const dHour = Math.floor((diff / (1000*60*60)) % 24);
+    const dMin = Math.floor((diff / (1000*60)) % 60);
+    const dSec = Math.floor(diff / 1000 % 60);
+    clockTitle.innerText = `${dDay}d ${dHour}h ${dMin}m ${dSec}s`;
 }
-
-gameForm.addEventListener("submit", onGameFormSubmit);
+clock()
+setInterval(clock, "1000");
